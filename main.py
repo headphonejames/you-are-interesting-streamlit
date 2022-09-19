6f4885fd6e8859936b098c0e8d64c010ff092f3a
James Kirsch <generalfuzz@gmail.com>
2022-09-19 15:52:49 -0700

    gsheets test

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
 import streamlit as st
+from google.oauth2 import service_account
+from gsheetsdb import connect
+
+# Create a connection object.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+sheet_url = st.secrets["private_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# Print results.
+for row in rows:
+    st.write(f"{row.name} has a :{row.pet}:")
+
 
 if 'workers' not in st.session_state:
     st.session_state.workers = ['james', 'not james']
