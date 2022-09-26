c9e83d9458f1c1f4bb3dcffce8b70d7fac16ebc8
James Kirsch <generalfuzz@gmail.com>
2022-09-26 16:02:35 -0700

    google sheets refactor 4

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import df_funcs as df_func
 
 #constants
 worker_table_name = "workers"
-
-
 def set_df(new_df):
     st.session_state.dataframe = new_df
 
@@ -13,46 +11,37 @@ def get_df():
     return st.session_state.dataframe
 
 # initialize worker list
-if 'workers' not in st.session_state:
+if 'dataframe' not in st.session_state:
     set_df(gsheets.load_the_table(worker_table_name))
-    print(get_df())
-    if (get_df().empty):
-        workers = []
-    else:
-        workers = get_df()["workers"].values.tolist()
-    st.session_state.workers = workers
-
-#local vars
-wlist = st.session_state.workers
 
 def remove_worker(worker, index):
     #Remove worker from list in data table
-    set_df(df_func.remove_col(get_df(), index-1))
-    #Remove worker name from ui list
-    wlist.remove(worker)
-    # update sheet
-    gsheets.update_the_table(get_df(), worker_table_name)
+    asdf = df_func.remove_col(get_df(), index)
+    set_df(asdf)
+    # set_df(df_func.remove_col(get_df(), index))
 
 def add_worker():
     worker_name = st.session_state.worker_name
-    if not worker_name in wlist:
-        # add to ui list
-        wlist.append(worker_name)
+    if not worker_name in get_df()["workers"]:
         #update datatable
         set_df(df_func.add_col(get_df(),  worker_name))
-        # update sheet
-        gsheets.update_the_table(get_df(), worker_table_name)
         # clear state
         st.session_state.worker_name = ''
 
 st.title('Who is staffing?')
 
 index = 0
-for worker in wlist:
+for worker in get_df()["workers"]:
     index = index + 1
     st.checkbox(worker, key="id_{}".format(worker),  on_change=remove_worker, args=(worker, index, ))
 
 col1, col2 = st.columns([1,1])
 
+st.session_state.worker_name = ''
 st.text_input(label="worker name", placeholder="worker name", on_change=add_worker, key='worker_name')
 
+def done():
+    # update sheet
+    gsheets.update_the_table(get_df(), worker_table_name)
+
+st.button("submit", on_click=done)
